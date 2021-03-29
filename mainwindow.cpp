1b5e95faf070f2c55a25c22dc505ab9f0ae8eae0
zaineb21 <61331889+zaineb21@users.noreply.github.com>
2021-03-29 11:28:39 +0100

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,20 +1,11 @@
-
-#include "ui_mainwindow.h"
-#include "cavalier.h"
-#include"QIntValidator"
 #include "mainwindow.h"
-#include"QMessageBox"
-
-
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+#include "ui_mainwindow.h"
+#include "planningcheval.h"
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->ID->setValidator(new QIntValidator(0,9999999,this));
-    ui->Id_2->setValidator(new QIntValidator(0,9999999,this));
-
-
 }
 
 MainWindow::~MainWindow()
@@ -22,79 +13,51 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+
+
+
+
+
+
 void MainWindow::on_ajouter_clicked()
 {
-    int ID=ui->ID->text().toInt ();
-    QString nom=ui->nom->text () ;
-    QString prenom=ui->prenom->text () ;
-    QString dateN=ui->dateN->text () ;
-    QString  dateI=ui->dateI->text () ;
-    QString type=ui->type->text () ;
-    QString Email=ui->Email->text () ;
-    QString nomE=ui->nomE->text () ;
-    int num=ui->num->text().toInt ();
-    QString maladie=ui->maladie->text () ;
-
- Cavalier c(ID,nom,prenom,dateN,dateI,type ,Email,nomE,num, maladie);
-
+        int Id_cheval=ui->Id_cheval->text().toInt ();
+        QString Nom_activite=ui->Nom_activite->text () ;
+        QString date_activite=ui->date_activite->text () ;
+        int duree=ui->duree->text().toInt ();
+        QString Id_cavalier=ui->Id_cavalier->text () ;
+        planningCheval P(Id_cheval,Nom_activite,date_activite,duree,Id_cavalier);
 
 }
-
 
 void MainWindow::on_afficher_clicked()
-{
-    Cavalier c ;
-    ui->tab_cavalier->setModel(c.afficher()) ;
+{ planningCheval P ;
+    ui->tab_planning->setModel(P.afficher()) ;
 
 }
-
-
 
 void MainWindow::on_modifier_clicked()
 {
-        int ID=ui->ID->text().toInt ();
-        QString nom=ui->nom->text () ;
-        QString prenom=ui->prenom->text () ;
-        QString dateN=ui->dateN->text () ;
-        QString  dateI=ui->dateI->text () ;
-        QString type=ui->type->text () ;
-        QString Email=ui->Email->text () ;
-        QString nomE=ui->nomE->text () ;
-        int num=ui->num->text().toInt ();
-        QString maladie=ui->maladie->text () ;
-         Cavalier c(ID,nom,prenom,dateN,dateI,type ,Email,nomE,num, maladie);
 
-    bool test1=c.ajouter();
-    if(test1)
-    {
-        QMessageBox::information(nullptr, QObject::tr("Modifier un cavalier"),
-                                 QObject::tr("cavalier  modifié.\n"
-                                             "Click Cancel to exit."), QMessageBox::Cancel);
-
-    }
-    else
-        QMessageBox::critical(nullptr, QObject::tr("Modifier un cavalier "),
-                              QObject::tr("Erreur !.\n"
-                                          "Click Cancel to exit."), QMessageBox::Cancel);
 }
-
 
 void MainWindow::on_supprimer_clicked()
 {
-    Cavalier  c1 ;
-    c1.setID(ui->Id_2->text().toInt());
-    bool test =c1.supprimer(c1.getID());
-    if(test)
-    {
-        QMessageBox::information(nullptr, QObject::tr("supprimer un cavalier"),
-                                 QObject::tr("cavalier  supprimé.\n"
-                                             "Click Cancel to exit."), QMessageBox::Cancel);
-        ui->tab_cavalier->setModel(c1.afficher()) ;
+    planningCheval  P ;
+        P.setId_cheval(ui->Id_cheval2->text().toInt());
+        bool test =P.supprimer(P.getId_cheval());
+        if(test)
+        {
+            QMessageBox::information(nullptr, QObject::tr("supprimer un cavalier"),
+                                     QObject::tr("cavalier  supprimé.\n"
+                                                 "Click Cancel to exit."), QMessageBox::Cancel);
+            ui->tab_cavalier->setModel(P.afficher()) ;
 
-    }
-    else
-        QMessageBox::critical(nullptr, QObject::tr("supprimer un cavalier "),
-                              QObject::tr("Erreur !.\n"
-                                          "Click Cancel to exit."), QMessageBox::Cancel);
+        }
+        else
+            QMessageBox::critical(nullptr, QObject::tr("supprimer un cavalier "),
+                                  QObject::tr("Erreur !.\n"
+                                              "Click Cancel to exit."), QMessageBox::Cancel);
 
 }
