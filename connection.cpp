8e0b515ea51ac9a5762c5568288caa7ffe4821db
mimi667 <mariem.aljene@esprit.tn>
2021-04-01 09:41:05 +0100

    hello

--- connection.cpp
+++ connection.cpp
@@ -8,7 +8,7 @@ Connection::Connection()
 bool Connection::createconnect()
 {bool test=false;
 QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-db.setDatabaseName("test");//inserer le nom de la source de données ODBC
+db.setDatabaseName("Test");//inserer le nom de la source de données ODBC
 db.setUserName("mariem");//inserer nom de l'utilisateur
 db.setPassword("esprit18");//inserer mot de passe de cet utilisateur
 
