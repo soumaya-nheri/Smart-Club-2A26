9f8bf859de23aa4a0ea6e656a26c55e7bccf78d1
zaineb triki <zaineb.triki@esprit.tn>
2021-04-26 22:25:36 +0100

    integration_Zaineb

---
+++ notifications.h
@@ -0,0 +1,13 @@
+#ifndef NOTIFICATIONS_H
+#define NOTIFICATIONS_H
+#include <QSystemTrayIcon>
+#include<QString>
+
+class notifications
+{
+public:
+    notifications();
+    void notifications_ajoutcheval();
+};
+
+#endif // NOTIFICATIONS_H
