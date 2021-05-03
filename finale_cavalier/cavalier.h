5f4dd2a1528e5ab1342c48ed12a81aebb46fae0e
mimi667 <mariem.aljene@esprit.tn>
2021-05-03 02:38:43 +0100

    h

---
+++ finale_cavalier/cavalier.h
@@ -0,0 +1,46 @@
+#ifndef CAVALIER_H
+#define CAVALIER_H
+#include <QString>
+#include <QSqlQuery>
+#include <QSqlQueryModel>
+#include"mainwindow.h"
+#include "QDate"
+
+class Cavalier
+{
+public:
+    Cavalier();
+    Cavalier(int,QString,QString,QString,QString,QString, QString, QString ,int ,QString );
+    int get_ID() ;
+    QString get_nom ();
+    QString get_prenom ();
+    QString  get_dateN ();
+    QString get_dateI();
+    QString get_type() ;
+    QString get_Email() ;
+    QString get_nomE();
+   int get_num() ;
+    QString  get_maladie() ;
+    
+
+
+
+
+    bool ajouter();
+        QSqlQueryModel * afficher();
+        bool supprimer(int);
+        bool modifier(int,QString,QString,QString,QString,QString, QString, QString ,int ,QString );
+        QSqlQueryModel * rechercher(QString);
+        QSqlQueryModel * rechercher_nom(QString);
+        QSqlQueryModel * trier();
+        QSqlQueryModel * trie();
+          QSqlQueryModel * trie_ID();
+
+private :
+
+int ID ,num ;
+QString nom , prenom, dateN,dateI,type , Email , nomE , maladie ;
+
+};
+
+#endif // CAVALIER_H
